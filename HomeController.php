85196d612001dcd32bdd4c0cc7726fc5aa4a7f43
MazenAudi <55948520+MazenAudi@users.noreply.github.com>
2023-10-27 13:25:43 +0300

    Add files via upload

---
+++ HomeController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+use App\Models\Food;
+
+use App\Models\Reservation;
+
+use App\Models\Foodchef;
+
+use App\Models\Cart;
+
+
+
+
+
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+
+        $data=food::all();
+        $data2=foodchef::all();
+        return view("home",compact("data","data2"));
+
+}
+
+
+    public function redirects()
+    {
+
+        $data=food::all();
+        $data2=foodchef::all();
+        $usertype= Auth::user()->usertype;
+
+        if($usertype=='1')
+        {
+            return view('admin.adminhome');
+        }
+
+        else {
+            return view('home',compact('data','data2'));
+        }
+    }
+
+        public function addcart(Request $request,$id)
+        {
+            if(Auth::id())
+            {
+
+                $user_id=Auth::id();
+
+                $foodid=$id;
+                $quantity=$request->quantity;
+
+                $cart = new cart;
+
+                $cart->user_id=$user_id;
+                $cart->food_id=$foodid;
+                $cart->quantity=$quantity;
+                $cart->save();
+
+          
+                return redirect()->back();
+
+            }
+            else{
+                return redirect('/login');
+            }
+        }
+}
+
